dcf9a852ae6d888847b1d00fc2e23f592f9fe729
Przemek Boniarczyk <boniar@gmail.com>
2018-12-12 21:57:36 +0100

    two

--- skrypt.sh
+++ skrypt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo 'Witaj w skrypcie realizującym cwiczenie polegajace na utworzeniu BD MySQL'
 echo 'Aktualizuje liste  dostepnych pakietow'
-sudo apt update
+#sudo apt update
 echo 'Przystepuje do instalacji serwera MySQL w najnowszej wersji'
-sudo apt install mysql-server
+#sudo apt install mysql-server
 echo 'Przeprowadzam konfiguracje dostepu'
-sudo mysql_secure_installation
+#sudo mysql_secure_installation
 echo 'Sprawdzam status procesu systemowego MySQL'
 sudo systemctl status mysql.service
 #sudo systemctl start mysql
