3a7f2e5119f7c7d89aa0b485f432406627727818
Przemek Boniarczyk <boniar@gmail.com>
2018-12-13 13:31:05 +0100

    insert_#2

--- insert_db.sql
+++ insert_db.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (User_id, Firstname, Lastname, Email, Password, Role, )
-VALUES ('1', 'Przemek', 'Boniarczyk', 'boniar@gmail.com', 'haslo123', 'admin');
+VALUES (1, 'Przemek', 'Boniarczyk', 'boniar@gmail.com', 'haslo123', admin);
 
 INSERT INTO posts (User_id, Post_id, Text, Date, )
-VALUES ('1', '1', 'To jest moj pierwszy,wspanialy post.', '2018-12-13');
+VALUES (1, 1, 'To jest moj pierwszy,wspanialy post.', '2018-12-13');
 
 INSERT INTO comment (User_id, Post_id, Comment_id, Text, Date, )
 VALUES ('1', '1', '1', 'Rewelacyjny post!', '2018-12-13'); 
--- skrypt.sh
+++ skrypt.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo 'Witaj w skrypcie realizującym cwiczenie polegajace na utworzeniu BD MySQL'
 echo 'Aktualizuje liste  dostepnych pakietow'
-#sudo apt update
+sudo apt update
 echo 'Przystepuje do instalacji serwera MySQL w najnowszej wersji'
-#sudo apt install mysql-server
+sudo apt install mysql-server
 echo 'Przeprowadzam konfiguracje dostepu'
-#sudo mysql_secure_installation
+sudo mysql_secure_installation
 echo 'Sprawdzam status procesu systemowego MySQL'
 sudo systemctl status mysql.service
-#sudo systemctl start mysql
-echo 'Logouje sie do serwera MySQL i wykonuje skrypt SQL'
+sudo systemctl start mysql
+echo 'Loguje sie do serwera MySQL i wykonuje skrypt SQL'
 sudo  mysql -u root -p < create_db.sql
 echo 'Skrypt wykonany pomyslnie.Pozdrawiam'
